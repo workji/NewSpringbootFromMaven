a2944a8904a1e273b4c28adf950b23061e502c48
workji910 <workji910@gmail.com>
2024-09-14 14:59:09 +0900

    first commit

---
+++ src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "FreeMarker Hello");
+        return "hello";
+    }
+
+}
